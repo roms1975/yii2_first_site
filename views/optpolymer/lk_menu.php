c5d0a98da1e3be689318742870cb314a447a0c3a
roms1975 <odggbo@gmail.com>
2020-05-12 23:51:48 +0300

    26

--- views/optpolymer/lk_menu.php
+++ views/optpolymer/lk_menu.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 	use yii\widgets\Menu;
 	
@@ -14,20 +13,3 @@
 		'activeCssClass'=>'active',
 	]);
 ?>
-=======
-<?php
-	use yii\widgets\Menu;
-	
-	echo Menu::widget([
-		'items' => [
-			['label' => 'Персональные данные', 'url' => ['optpolymer/showperson'] ],
-			['label' => 'Заказы', 'url' => ['optpolymer/orders']],
-			['label' => 'Выход', 'url' => ['logout']],
-		],
-		'options' => [
-			'class' => 'lk-menu',
-		],
-		'activeCssClass'=>'active',
-	]);
-?>
->>>>>>> 2ab52489858bb854a784b9997c35ba5b392579ca
